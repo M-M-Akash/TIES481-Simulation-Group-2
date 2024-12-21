781e7e0ddc470bb302e30a0ad5cdb4532069eb83
Akash-Rayhan <akash18rayhan@gmail.com>
2024-12-21 18:27:48 +0200

    updated files

--- Assignment_4.py
+++ Assignment_4.py
@@ -1,11 +1,62 @@
 import simpy
 import numpy as np
+import random
 from monitoring import Monitor
-from simulation import HospitalSimulation
 import itertools
 from scipy.stats import pearsonr
 from sklearn.linear_model import LinearRegression
 
+
+
+class HospitalSimulation:
+    def __init__(self, env, num_prep, num_recovery, interarrival_dist, prep_time_dist, recovery_time_dist, seed=None):
+        random.seed(seed)
+        self.env = env
+        self.preparation = simpy.PriorityResource(env, num_prep)
+        self.operating_theater = simpy.Resource(env, 1)
+        self.recovery = simpy.Resource(env, num_recovery)
+        self.queue_lengths = {"preparation": [], "operation": [], "recovery": []}
+        self.interarrival_dist = interarrival_dist
+        self.prep_time_dist = prep_time_dist
+        self.recovery_time_dist = recovery_time_dist
+        self.time_snapshots = []
+        self.theater_blocked_times = []
+        self.theater_busy_start = None
+
+    def preparation_process(self, patient_id, priority):
+        with self.preparation.request(priority=priority) as request:
+            yield request
+            yield self.env.timeout(next(self.prep_time_dist))
+
+    def operation_process(self, patient_id):
+        with self.operating_theater.request() as request:
+            yield request
+            self.theater_busy_start = self.env.now
+            yield self.env.timeout(np.random.exponential(20))
+            if len(self.recovery.queue) >= 1:
+                self.theater_blocked_times.append(self.env.now - self.theater_busy_start)
+            self.theater_busy_start = None
+
+    def recovery_process(self, patient_id):
+        with self.recovery.request() as request:
+            yield request
+            yield self.env.timeout(next(self.recovery_time_dist))
+
+    def patient_lifecycle(self, patient_id, priority=1):
+        yield self.env.process(self.preparation_process(patient_id, priority))
+        yield self.env.process(self.operation_process(patient_id))
+        yield self.env.process(self.recovery_process(patient_id))
+
+    def generate_patients(self, warm_up):
+        patient_id = 0
+        while True:
+            if self.env.now >= warm_up:
+                priority = 0 if random.random() < 0.2 else 1
+            else:
+                priority = 1  # General patients during warm-up
+            yield self.env.timeout(next(self.interarrival_dist))
+            self.env.process(self.patient_lifecycle(patient_id, priority))
+            patient_id += 1
 
 # Run experiments
 warm_up_time = 200
